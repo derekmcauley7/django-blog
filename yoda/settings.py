520312b7768490345cc43c933e5a40f08c9fc32a
derekmcauley <derekmcauley7@gmail.com>
2023-11-19 15:43:58 +0000

    Adding WhiteNoise

--- yoda/settings.py
+++ yoda/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
